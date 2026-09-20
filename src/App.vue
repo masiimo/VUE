b80bef85fc8db9f9e25446a9cd71715d779b5726
masiimo <usum439@gmail.com>
2026-09-20 11:53:55 +0200

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+
+</script>
+
+
+<template>
+
+  <h1>Vue + TS</h1>
+
+  <ConstLet />
+
+</template>
+
+
+
---
+++ src/typescript/ConstLet.vue
@@ -0,0 +1,16 @@
+
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div>
+        <h2>Tipos básicos</h2>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
